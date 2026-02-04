30205dcc947de6031160ac497be5e194d5a008d7
ahmed5100fa <ahmed5722144@gmail.com>
2026-02-04 22:40:44 +0200

    reusable phone input added.

--- apps/ecommerce/src/app/app.ts
+++ apps/ecommerce/src/app/app.ts
@@ -4,9 +4,9 @@ import { NxWelcome } from './nx-welcome';
 import { FormsModule } from '@angular/forms';
 import { SliderModule } from 'primeng/slider';
 import {AuthLibraryService } from '@org/auth';
-import { FormInput } from "./shared/components/form-input/form-input";
+import { PhoneInput } from "./shared/components/phone-input/phone-input";
 @Component({
-  imports: [RouterModule, SliderModule, FormsModule, FormInput],
+  imports: [RouterModule, SliderModule, FormsModule, PhoneInput],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
