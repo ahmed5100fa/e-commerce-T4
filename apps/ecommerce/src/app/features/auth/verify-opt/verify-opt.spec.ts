6724e835af37883f597e43c74279c0ca36771d45
youssefhaitham <youssefhaitham2111@gmail.com>
2026-02-10 19:22:17 +0200

    the edit in ui

---
+++ apps/ecommerce/src/app/features/auth/verify-opt/verify-opt.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { VerifyOpt } from './verify-opt';
+
+describe('VerifyOpt', () => {
+  let component: VerifyOpt;
+  let fixture: ComponentFixture<VerifyOpt>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [VerifyOpt],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(VerifyOpt);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
