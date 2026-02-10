c14585f14edcdcb5b422114885ee6a9eeb4397e0
youssefhaitham <youssefhaitham2111@gmail.com>
2026-02-10 16:16:03 +0200

    finish login feature

--- apps/ecommerce/src/app/features/auth/logincomponent/logincomponent.ts
+++ apps/ecommerce/src/app/features/auth/logincomponent/logincomponent.ts
@@ -1,16 +1,71 @@
-import { Component,  } from '@angular/core';
-import {  RouterModule } from "@angular/router";
+import { loginData } from './../../../../../../../libs/auth/src/lib/interfaces/auth-data';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { FormInput } from '../../../shared/components/form-input/form-input';
-import { PhoneInput } from "../../../shared/components/phone-input/phone-input";
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthApiAdaptorService } from 'libs/auth/src/lib/adaptor/auth-api.adaptor';
+import { AuthLibraryService } from '@org/auth';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
-  imports: [RouterModule, FormInput,],
+  imports: [RouterModule, FormInput, FormsModule, ReactiveFormsModule],
 })
 export class LoginComponent {
+  private readonly _authLibraryService = inject(AuthLibraryService);
+  private readonly _router = inject(Router);
+  msgSuccess: string = '';
+  msgError: string = '';
 
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+      ),
+    ]),
+  });
+
+ 
+
+
+ //test
+
+ submitForm(): void {
+  if (this.loginForm.valid) {
+
+    this._authLibraryService.login(this.loginForm.value as any).subscribe({
+      next: (res: any) => {
+        console.log(res);
+
+        if (res.message === 'success') {
+          this.msgSuccess = 'Logged in successfully';
+          this.msgError = '';
+
+          setTimeout(() => {
+            
+            inject(Router).navigate(['/home']);
+          }, 2000);
+        }
+      },
+
+      error: (err: any) => {
+        console.log(err);
+        this.msgError = err.error.message;
+      },
+    });
+
+  }
 }
 
+
+}
