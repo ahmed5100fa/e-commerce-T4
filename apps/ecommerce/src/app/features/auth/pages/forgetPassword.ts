711fd7e8b1ad41af402748c0b196f38b841bb856
sondosEssam <sondosessam713@gmail.com>
2026-02-10 19:26:55 +0200

    tailwind minor erre fix, forget passowrd initail ui

---
+++ apps/ecommerce/src/app/features/auth/pages/forgetPassword.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormInput } from "../../../shared/components/form-input/form-input";
+
+@Component({
+  selector: 'app-forget-password',
+  imports: [FormInput],
+  templateUrl: './forgetPassword.html',
+  styleUrls: ['./forgetPassword.scss'],
+})
+export class ForgetPassword {
+
+
+}
