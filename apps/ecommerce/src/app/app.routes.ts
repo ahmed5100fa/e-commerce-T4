5c50dbc7128853f5260a57ff64114a61c73f0510
youssefhaitham <youssefhaitham2111@gmail.com>
2026-02-23 21:03:18 +0200

    update login UI

--- apps/ecommerce/src/app/app.routes.ts
+++ apps/ecommerce/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Route } from '@angular/router';
 import { authRouters } from './features/auth/auth.routers';
+import { LoginComponent } from './features/auth/pages/logincomponent/logincomponent';
+import { Authlayout } from './layouts/auth layout/authlayout';
 export const appRoutes: Route[] = [
   {
     path: '',
     component: Authlayout,
     children: [
-      { path: 'login', component: LoginComponent },
+      { path: 'login', component:LoginComponent },
       {
         path: 'register',
         loadComponent: () =>
