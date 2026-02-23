44b9d8d074661c8726f98eaff644653f82053b75
sondosEssam <sondosessam713@gmail.com>
2026-02-23 21:56:33 +0200

    main layout + footer

---
+++ apps/ecommerce/src/app/layouts/main-layout/components/footer/footer.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormInput } from '../../../../shared/components/form-input/form-input';
+import { CustomButton } from '@Ui-components';
+import { LucideAngularModule, ArrowRight  } from 'lucide-angular';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  imports: [CustomButton, LucideAngularModule, RouterLink],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
+  host:{
+  '[attr.data-theme]':'"dark"'
+  }
+})
+export class Footer {
+readonly ArrowRight = ArrowRight;
+}
