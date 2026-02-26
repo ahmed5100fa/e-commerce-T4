d1697fe2d781c65743c51bb3ab6ab4ac9cc66f02
ahmed5100fa <ahmed5722144@gmail.com>
2026-02-26 21:32:12 +0200

    solve conflict

--- apps/ecommerce/src/app/features/Home/home.ts
+++ apps/ecommerce/src/app/features/Home/home.ts
@@ -1,34 +1,9 @@
-import { Component, inject } from '@angular/core';
-import { Navbar } from "../../layouts/main layout/navbar/navbar";
-import { BestSelling } from "./components/best-selling/best-selling";
-import { ProductService } from './services/ProductService/product-service';
-import { gatAllProducts, Product } from '../../shared/interfaces/card-product';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'home-page',
-  imports: [Navbar, BestSelling],
+  imports: [],
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
 })
-export class HomePageComponent {
-
-   products: Product[] = [];
-
-  private productService = inject(ProductService);
-
-  getProducts() {
-  this.productService.getProducts().subscribe((res: gatAllProducts) => {
-    this.products = [...res.products];
-
-    setTimeout(() => {
-      window.dispatchEvent(new Event('resize'));
-    }, 0);
-  });
-}
-
-  ngOnInit() {
-    this.getProducts();
-  }
-
-
-}
+export class HomePageComponent {}
