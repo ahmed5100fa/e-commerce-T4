ee5d6ccb15ccb96444e91bcb9985157b43395ef5
ahmed5100fa <ahmed5722144@gmail.com>
2026-02-28 15:12:47 +0200

    delete flowbite

--- apps/ecommerce/src/app/features/Home/home.ts
+++ apps/ecommerce/src/app/features/Home/home.ts
@@ -1,12 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AboutUs } from './components/about-us/about-us';
 import { NgOptimizedImage } from '@angular/common';
+import { Navbar } from "../../layouts/main layout/navbar/navbar";
 
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [AboutUs,],
-  templateUrl: './home.html',
+  imports: [AboutUs, Navbar],
+  templateUrl: `./home.html`,
   styleUrls: ['./home.scss'],
 })
 export class HomePageComponent {}
