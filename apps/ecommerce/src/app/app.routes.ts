2ebfce09a8a8591733a89f946dd22021e4b3939c
youssefhaitham <youssefhaitham2111@gmail.com>
2026-03-05 17:24:38 +0200

    the edit in product details

--- apps/ecommerce/src/app/app.routes.ts
+++ apps/ecommerce/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { authRouters } from './features/auth/auth.routers';
 import { mainRouter } from './layouts/main-layout/main.routes';
 import { ProductDet } from './features/Home/components/product-det/product-det';
 export const appRoutes: Route[] = [...authRouters, ...mainRouter,
+
+    // ===>> To see the output only <<===
     {path:'product/:id/:slug',component:ProductDet}
 
 
--- apps/ecommerce/src/app/features/Home/components/product-det/product-det.ts
+++ apps/ecommerce/src/app/features/Home/components/product-det/product-det.ts
@@ -1,12 +1,14 @@
+import { ProductService } from './../../services/ProductService/product-service';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ProductService } from '../../services/ProductService/product-service';
+
 import {
   Metadata,
   Product,
 } from 'apps/ecommerce/src/app/shared/interfaces/card-product';
 import { CustomButton } from '@Ui-components';
 import { NgOptimizedImage } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-product-det',
@@ -20,14 +22,29 @@ export class ProductDet implements OnInit {
   id: string | null = null;
   //create interface for product details and use it instead of object!!
   productDetails: Product = {} as Product;
+  //current image
+  currentImage!: string;
 
   ngOnInit(): void {
     this.getProductId();
     this.getProductDetailsData();
+    //currant image
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.productService
+        .getProducts()
+        .pipe(takeUntilDestroyed())
+        .subscribe((res: any) => {
+          this.productDetails = res.data;
+          if (this.productDetails?.images?.length) {
+            this.currentImage = this.productDetails.images[0];
+          }
+        });
+    }
   }
 
   getProductId(): void {
-    this.activatedRoute.paramMap.subscribe({
+    this.activatedRoute.paramMap.pipe(takeUntilDestroyed()).subscribe({
       next: (params) => {
         console.log(params.get('id'));
         this.id = params.get('id');
@@ -36,17 +53,22 @@ export class ProductDet implements OnInit {
   }
 
   getProductDetailsData(): void {
-    this.productService.getProducts().subscribe({
-      next: (res) => {
-        console.log(res.metadata);
-        const product = res.products.find((p: Product) => p._id === this.id);
-        if (product) {
-          this.productDetails = product;
-        }
-      },
-      error: (error) => {
-        console.error(error);
-      },
-    });
+    this.productService
+      .getProducts()
+      .pipe(takeUntilDestroyed())
+      .subscribe({
+        next: (res) => {
+          console.log(res.metadata);
+          const product = res.products.find((p: Product) => p._id === this.id);
+          if (product) {
+            this.productDetails = product;
+          }
+        },
+        error: (error) => {
+          console.error(error);
+        },
+      });
   }
+
+  
 }
