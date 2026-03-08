641c0d20f856b0c98374e274d0a8ef9bbe9d6c96
sondosEssam <sondosessam713@gmail.com>
2026-03-08 06:40:37 +0200

    product review ui done

--- apps/ecommerce/src/app/features/products/components/product-review/product-review.ts
+++ apps/ecommerce/src/app/features/products/components/product-review/product-review.ts
@@ -1,28 +1,67 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, effect, inject, input, signal } from '@angular/core';
 import { ProductsService } from '../../products.service';
 import { Reviews } from 'apps/ecommerce/src/app/shared/interfaces/reviews';
 import { SecondHeader } from "apps/ecommerce/src/app/shared/components/secondHeader/secondHeader";
 import { RatingModule } from 'primeng/rating';
-import { FormsModule } from '@angular/forms';
-
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { TextareaModule } from 'primeng/textarea';
+import { FormGroup } from '@angular/forms';
+import { FormInput } from 'apps/ecommerce/src/app/shared/components/form-input/form-input';
+import { CustomButton } from "@Ui-components";
 @Component({
   selector: 'app-product-review',
-  imports: [SecondHeader, RatingModule, FormsModule],
+  imports: [SecondHeader, RatingModule, FormsModule, DatePipe, FormInput, TextareaModule, ReactiveFormsModule, CustomButton],
   templateUrl: './product-review.html',
-  styleUrl: './product-review.css',
+  styleUrl: './product-review.scss',
 })
 export class ProductReview {
   _productService = inject(ProductsService);
   productId = input<string>('');
-  reviews: Reviews[] = [];
-  generalRating: number = 0.0;
+  reviews = signal<Reviews[]>([]);
+  rating:number = 0;
+
+  isloggedIn = signal(false); //temprorary until we have implemented
+
+
+  formBuilder = inject(FormBuilder);
+
+
+  reviewForm = this.formBuilder.group({
+    title: [''],
+    review: [''],
+    rating: [0]
+  });
+
+
+
+  generalRating = computed(() => {
+    const reviewsList = this.reviews();
+    if (reviewsList.length === 0) return 0;
+    
+    const totalRating = reviewsList.reduce((sum, review) => sum + review.rating, 0);
+    return totalRating / reviewsList.length;
+  });
+
+
   ngOnInit(){
     this._productService.getProductReview(this.productId()).subscribe((res:any)=>{
-      this.reviews = res.reviews;
-      // Calculate general rating
-      if (this.reviews.length > 0) {
-        const totalRating = this.reviews.reduce((sum, review) => sum + review.rating, 0);
-        this.generalRating = totalRating / this.reviews.length;
-      }
+      this.reviews.set(res.reviews);
     });
-}}
+}
+onSubmit(){
+  // this part won't do anything for now as said in the video
+  if(this.reviewForm.valid){
+    const newReview = {
+      title: this.reviewForm.value.title!,
+      comment: this.reviewForm.value.review!,
+      rating: this.reviewForm.value.rating!,
+      createdAt: new Date().toISOString()
+    };
+    this.reviewForm.reset();
+  }
+}
+
+
+}
+
