7ab20b8848175d93d96c983ff6da9302d19c7a5a
ahmed5100fa <ahmed5722144@gmail.com>
2026-03-09 23:14:33 +0200

    edit some sections

--- apps/ecommerce/src/app/features/Home/home.ts
+++ apps/ecommerce/src/app/features/Home/home.ts
@@ -1,17 +1,18 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AboutUs } from './components/about-us/about-us';
-import { NgOptimizedImage, NgClass } from '@angular/common';
 import { SpecialGiftsCard } from "../../shared/components/special-gifts-card/special-gifts-card";
 import { FeatureBar } from "./components/feature bar/feature-bar";
 import { BestSelling } from "./components/best-selling/best-selling";
 import { TrustedBy } from "./components/trusted-by/trusted-by";
 import { Product } from '../../shared/interfaces/card-product';
-import { AuthLibraryService } from '@org/auth';
 import { ProductService } from './services/ProductService/product-service';
+import { MostPopularSection } from "./components/Most Popular/most-popular";
+import { TestimonialsSection } from "./components/Testimonials/testimonials";
+import { SpecialGiftsComponent } from "./components/special gifts/special-gifts";
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [SpecialGiftsCard, FeatureBar, BestSelling, TrustedBy, NgClass],
+  imports: [SpecialGiftsCard, FeatureBar, BestSelling, TrustedBy, MostPopularSection, TestimonialsSection, SpecialGiftsComponent, AboutUs],
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
 })
