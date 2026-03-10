32d4a62eeea86758057ffa346d3c8725dbb7839c
ahmed5100fa <ahmed5722144@gmail.com>
2026-03-10 15:51:21 +0200

    edit some conflicts

--- apps/ecommerce/src/app/layouts/main-layout/main.routes.ts
+++ apps/ecommerce/src/app/layouts/main-layout/main.routes.ts
@@ -1,28 +1,5 @@
 import { Route } from '@angular/router';
 
-
-export const mainRouter: Route[] = [
-  {
-    path: 'main',
-    loadComponent: () =>
-      import('../../layouts/main-layout/main-layout').then((m) => m.MainLayout),
-    children: [
-      {
-        path: 'home',
-        loadComponent: () =>
-          import('../../features/Home/home').then((m) => m.HomePageComponent),
-      },
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-    ],
-  },
-  {
-    path: 'product/:id',
-    loadComponent: () =>
-      import('../../features/Home/components/product-det/product-det').then(
-        (m) => m.ProductDet
-      ),
-  },
-
 import { productsRoutes } from '../../features/products/products.routes';
 export const mainRouter :Route[]  = [
     {path: 'main', loadComponent:()=>import('../../layouts/main-layout/main-layout').then(m=>m.MainLayout),
